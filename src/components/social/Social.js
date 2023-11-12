3576a4d8101a572a8956aeee1d6627c2f151b38f
SnyDrive ~ Sunil <sunil.goulamhoussen@edu.devinci.fr>
2023-11-12 16:35:24 +0100

    facebook social link

--- src/components/social/Social.js
+++ src/components/social/Social.js
@@ -8,7 +8,7 @@ function Social() {
                 <h1>Suis nous sur les réseaux sociaux</h1>
                 <div className="social-btns" data-aos="zoom-in">
                     <a href="https://www.instagram.com/vinciecodrive/" target="_blank" rel="noreferrer"><i class="fa fa-instagram"></i></a>
-                    <a href="https://www.instagram.com/vinciecodrive/" target="_blank" rel="noreferrer"><i class="fa fa-facebook"></i></a>
+                    <a href="https://www.facebook.com/vinciecodrive/" target="_blank" rel="noreferrer"><i class="fa fa-facebook"></i></a>
                     <a href="https://twitter.com/vinciecodrive" target="_blank" rel="noreferrer"><i class="fa fa-twitter"></i></a>
                     <a href="https://www.linkedin.com/company/vinci-eco-drive/" target="_blank" rel="noreferrer"><i class="fa fa-linkedin"></i></a>
                     <a href="https://www.youtube.com/channel/UCP3mySrdiInG_bHe5uk_Xog" target="_blank" rel="noreferrer"><i class="fa fa-youtube-play"></i></a>
